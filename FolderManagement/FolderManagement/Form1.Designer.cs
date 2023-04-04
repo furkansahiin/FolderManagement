04b25e4e53ac4a3e2335f9b0bc2b4a2f841336e0
furkansahiin <furkaniha@gmail.com>
2023-04-04 14:18:31 +0300

    init

---
+++ FolderManagement/FolderManagement/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace FolderManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonsource = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sourceText = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.changeSave = new System.Windows.Forms.CheckBox();
+            this.subdirectorieszipped = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.zipLevel = new System.Windows.Forms.ComboBox();
+            this.uzerineyaz = new System.Windows.Forms.RadioButton();
+            this.rename = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttontarget = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.targetText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonsource
+            // 
+            this.buttonsource.Location = new System.Drawing.Point(253, 63);
+            this.buttonsource.Name = "buttonsource";
+            this.buttonsource.Size = new System.Drawing.Size(36, 23);
+            this.buttonsource.TabIndex = 25;
+            this.buttonsource.Text = "...";
+            this.buttonsource.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 19);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Kaynak Klasörü Seçiniz";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(188, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 39);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Çevir ve Taşı";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // sourceText
+            // 
+            this.sourceText.Location = new System.Drawing.Point(16, 92);
+            this.sourceText.Name = "sourceText";
+            this.sourceText.Size = new System.Drawing.Size(277, 20);
+            this.sourceText.TabIndex = 22;
+            // 
+            // changeSave
+            // 
+            this.changeSave.AutoSize = true;
+            this.changeSave.Location = new System.Drawing.Point(60, 266);
+            this.changeSave.Name = "changeSave";
+            this.changeSave.Size = new System.Drawing.Size(164, 17);
+            this.changeSave.TabIndex = 36;
+            this.changeSave.Text = "Değerleri app.config\'e kaydet";
+            this.changeSave.UseVisualStyleBackColor = true;
+            // 
+            // subdirectorieszipped
+            // 
+            this.subdirectorieszipped.AutoSize = true;
+            this.subdirectorieszipped.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.subdirectorieszipped.Location = new System.Drawing.Point(60, 241);
+            this.subdirectorieszipped.Name = "subdirectorieszipped";
+            this.subdirectorieszipped.Size = new System.Drawing.Size(130, 19);
+            this.subdirectorieszipped.TabIndex = 35;
+            this.subdirectorieszipped.Text = "Alt Dizinler de ziple";
+            this.subdirectorieszipped.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(15, 308);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Sıkıştırılma düzeyi";
+            // 
+            // zipLevel
+            // 
+            this.zipLevel.AutoCompleteCustomSource.AddRange(new string[] {
+            "1",
+            "2",
+            "3"});
+            this.zipLevel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.zipLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.zipLevel.FormattingEnabled = true;
+            this.zipLevel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.zipLevel.Items.AddRange(new object[] {
+            "Normal",
+            "Sıkıştır",
+            "Sıkıştırma"});
+            this.zipLevel.Location = new System.Drawing.Point(18, 324);
+            this.zipLevel.Name = "zipLevel";
+            this.zipLevel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.zipLevel.Size = new System.Drawing.Size(104, 21);
+            this.zipLevel.TabIndex = 33;
+            // 
+            // uzerineyaz
+            // 
+            this.uzerineyaz.AutoSize = true;
+            this.uzerineyaz.Checked = true;
+            this.uzerineyaz.Location = new System.Drawing.Point(36, 209);
+            this.uzerineyaz.Name = "uzerineyaz";
+            this.uzerineyaz.Size = new System.Drawing.Size(80, 17);
+            this.uzerineyaz.TabIndex = 32;
+            this.uzerineyaz.TabStop = true;
+            this.uzerineyaz.Text = "Üzerine yaz";
+            this.uzerineyaz.UseVisualStyleBackColor = true;
+            // 
+            // rename
+            // 
+            this.rename.AutoSize = true;
+            this.rename.Location = new System.Drawing.Point(158, 209);
+            this.rename.Name = "rename";
+            this.rename.Size = new System.Drawing.Size(105, 17);
+            this.rename.TabIndex = 31;
+            this.rename.Text = "Yeniden Adlandır";
+            this.rename.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(13, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 15);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Hedefte aynı dosya varsa ?";
+            // 
+            // buttontarget
+            // 
+            this.buttontarget.Location = new System.Drawing.Point(253, 123);
+            this.buttontarget.Name = "buttontarget";
+            this.buttontarget.Size = new System.Drawing.Size(36, 23);
+            this.buttontarget.TabIndex = 29;
+            this.buttontarget.Text = "...";
+            this.buttontarget.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 19);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Hedef Klasörü Seçiniz";
+            // 
+            // targetText
+            // 
+            this.targetText.Location = new System.Drawing.Point(16, 152);
+            this.targetText.Name = "targetText";
+            this.targetText.Size = new System.Drawing.Size(277, 20);
+            this.targetText.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(21, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(259, 26);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Folder to Zip Converter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(311, 367);
+            this.Controls.Add(this.buttonsource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sourceText);
+            this.Controls.Add(this.changeSave);
+            this.Controls.Add(this.subdirectorieszipped);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.zipLevel);
+            this.Controls.Add(this.uzerineyaz);
+            this.Controls.Add(this.rename);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttontarget);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.targetText);
+            this.Controls.Add(this.label3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Folder Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonsource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox sourceText;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox changeSave;
+        private System.Windows.Forms.CheckBox subdirectorieszipped;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox zipLevel;
+        private System.Windows.Forms.RadioButton uzerineyaz;
+        private System.Windows.Forms.RadioButton rename;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttontarget;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox targetText;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FolderManagement/FolderManagement/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace FolderManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sourceText.Text.Trim() == "" || targetText.Text.Trim() == "")
+            {
+                // Dosya Yolları Boş
+                // File Paths are Empty
+                MessageBox.Show("Dosya yollarını belirleyiniz...");
+                myloggertxt("Hata", "Mesajı", false, "Dosya Yolları Boş");
+            }
+            else if (!Directory.Exists(sourceText.Text))
+            {
+                // Verilen Kaynak Dosya Yolu Bulunamadı
+                // Given Source File Path Not Found
+                MessageBox.Show("Girilen Kaynak Dosya Yolu Bulunamadı.");
+                myloggertxt("Hata", "Mesajı", false, "Girilen Kaynak Dosya Yolu Bulunamadı..");
+            }
+
+            else
+            {
+                myloggertxt("Bilgilendirme", "Mesajı", true, "İşlem Başlatılıyor...");
+
+                if (!Directory.Exists(targetText.Text))
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(targetText.Text);
+                }
+                int compressionlevel = zipLevel.SelectedIndex;
+                string sourcePath = sourceText.Text;
+                string targetPath = targetText.Text;
+
+                string[] subdirectories = Directory.GetDirectories(sourcePath);
+                string filePathName = new DirectoryInfo(sourceText.Text).Name;
+                string zipFilePathName = filePathName + ".zip";
+                if (changeSave.Checked)
+                {
+                    string sessionanadizin = sourceText.Text;
+                    string sessiontargetpath = targetText.Text;
+                    bool isYenidenAdlandir = rename.Checked;
+                    bool isAltDizin = subdirectorieszipped.Checked;
+                    SaveSettings(sessionanadizin, sessiontargetpath, isYenidenAdlandir, isAltDizin);
+                }
+
+                if (subdirectorieszipped.Checked)
+                {
+                    // alt dizinlerin de sıkıştırılması istendiğinde çalışacaktır.
+                    // will work when subdirectories are also requested to be zipped.
+                    string[] files = Directory.GetFiles(sourcePath);
+                    if (files.Length == 0)
+                    {
+                        myloggertxt(zipFilePathName, sourcePath, true, "Ana Dizine Ait Dosya Yok.");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            if (File.Exists(zipFilePathName))
+                            {
+                                File.Delete(zipFilePathName);
+                            }
+
+                            files = Array.FindAll(files, s => !Directory.Exists(s));
+
+                            using (ZipArchive zip = ZipFile.Open(zipFilePathName, ZipArchiveMode.Create))
+                            {
+                                foreach (string file in files)
+                                {
+                                    zip.CreateEntryFromFile(file, Path.GetFileName(file));
+                                }
+                            }
+
+                            moveFile(zipFilePathName);
+                            myloggertxt(zipFilePathName, sourcePath, true, "Ana Dizine Ait Dosyalar Ziplendi.");
+                        }
+                        catch
+                        {
+                            myloggertxt(zipFilePathName, sourcePath, false, "Ana Dizine Ait Dosyalar Alınamadı.");
+                        }
+                    }
+                }
+
+                if (compressionlevel == 2)
+                {
+                    myloggertxt("Bilgilendirme", "Mesajı", true, $"sıkıştırılmama düzeyi seçildi {compressionlevel}");
+
+                    if (Directory.Exists(sourcePath))
+                    {
+                        // Mevcut değilse, hedef dizin oluştur
+                        //Create target directory if it doesn't exist
+                        if (!Directory.Exists(targetPath))
+                        {
+                            Directory.CreateDirectory(targetPath);
+                        }
+
+                        // Kaynak dizindeki tüm dosyaları ve alt dizinleri alın
+                        // Get all files and subdirectories in source directory
+                        var files = new DirectoryInfo(sourcePath).GetFileSystemInfos("*.*", SearchOption.AllDirectories);
+
+                        //Dosyalar arasında dolaşın ve hedef klasöre taşıyın
+                        //Loop through files and Move to destination folder
+                        foreach (var file in files)
+                        {
+                            // Geçerli dosya/dizin için yeni hedef yolu oluşturun
+                            //Construct new target path for current file/directory
+                            var newTargetPath = targetPath + file.FullName.Replace(sourcePath, "");
+
+                            if (file.Attributes.HasFlag(FileAttributes.Directory))
+                            {
+                                // Hedef yolda dizin oluştur
+                                //Create directory in target path
+                                Directory.CreateDirectory(newTargetPath);
+                            }
+                            else
+                            {
+                                // Dosyayı hedef yola kopyala
+                                //Copy file to target path
+                                File.Copy(file.FullName, newTargetPath, true);
+                            }
+
+                            // Dosya/dizinin hedef yola başarıyla kopyalanıp kopyalanmadığını kontrol edin
+                            //Check if file/directory was successfully copied to target path
+                            if (File.Exists(newTargetPath) || Directory.Exists(newTargetPath))
+                            {
+                                myloggertxt(file.FullName, newTargetPath, true, "Zipsiz taşıma işlemi başarılı oldu.");
+                            }
+                            else
+                            {
+                                myloggertxt(file.FullName, newTargetPath, false, "Zipsiz taşıma işlemi başarısız oldu.");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    myloggertxt("Bilgilendirme", "Mesajı", true, $"sıkıştırılma düzeyi seçildi {compressionlevel}");
+
+                    foreach (string subdirectory in subdirectories)
+                    {
+                        string zipFilePath = Path.Combine(sourcePath, $"{Path.GetFileName(subdirectory)}.zip");
+                        if (File.Exists(zipFilePath))
+                        {
+                            File.Delete(zipFilePath);
+                        }
+
+                        ZipFile.CreateFromDirectory(subdirectory, zipFilePath, (CompressionLevel)compressionlevel, true);
+
+                        moveFile(zipFilePath);
+                    }
+                }
+
+                // process completed
+                MessageBox.Show("işlem tamamlandı.");
+            }
+        }
+        private void moveFile(string zipFilePath)
+        {
+            string currentPath = targetText.Text;
+            string fileName = Path.GetFileName(zipFilePath);
+            string targetFilePath = Path.Combine(currentPath, fileName);
+
+            if (rename.Checked)
+            {
+                try
+                {
+                    // yeniden adlandırma seçildiğinde işlenecektir
+                    //will be processed when rename is selected
+
+                    // şuanki zamana göre yıl-ay-gün-saat-dakika-saniye olarak alıp dosuya isminin sonuna ekleme yapar 
+                    // It takes the year-month-day-hour-minute-second according to the current time and adds it to the end of the file name.
+                    string _fileName = Path.GetFileNameWithoutExtension(targetFilePath);
+                    string extension = Path.GetExtension(targetFilePath);
+                    string newFileName = _fileName + "-" + DateTime.Now.ToString("yyyyMMddHHmmss") + extension;
+                    string newFilePath = Path.Combine(Path.GetDirectoryName(targetFilePath), newFileName);
+
+                    try
+                    {
+                        File.Copy(zipFilePath, newFilePath, true);
+                        if (File.Exists(newFilePath))
+                        {
+                            File.Delete(zipFilePath);
+                            myloggertxt(zipFilePath, newFilePath, true, "yeniden adlandırma ve Dosya taşıma işlemi başarılı oldu.");
+                        }
+                        else
+                        {
+                            myloggertxt(zipFilePath, newFilePath, false, "yeniden adlandırma ve Dosya kopyalama hatası: Dosya kopyalanamadı.");
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        myloggertxt(zipFilePath, newFilePath, false, $"yeniden adlandırılma ve Dosya taşıma işlemi başarısız oldu. hata : {ex}");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //hata durumunda günlüğe yazılacak
+                    // will be written to the log in case of error
+                    myloggertxt(zipFilePath, targetFilePath, false, $"yeniden adlandırılma başarısız. Hata : {ex.Message}");
+                }
+            }
+            if (uzerineyaz.Checked)
+            {
+                try
+                {
+                    try
+                    {
+                        // kopyalama işlemi yapar taşma fonksiyonu çağrılmaz
+                        // where the copy operation is performed, the overflow function is not called
+                        File.Copy(zipFilePath, targetFilePath, true);
+                        if (File.Exists(targetFilePath))
+                        {
+                            File.Delete(zipFilePath);
+                            myloggertxt(zipFilePath, targetFilePath, true, "klasik işlem ve Dosya taşıma işlemi başarılı oldu.");
+                        }
+                        else
+                        {
+                            myloggertxt(zipFilePath, targetFilePath, false, "klasik işlem ve Dosya kopyalama hatası: Dosya kopyalanamadı.");
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        //hata durumunda günlüğe yazılacak
+                        // will be written to the log in case of error
+                        myloggertxt(zipFilePath, targetFilePath, false, $"klasik işlem ve Dosya taşıma işlemi başarısız oldu. hata : {ex}");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //hata durumunda günlüğe yazılacak
+                    // will be written to the log in case of error
+
+                    myloggertxt(zipFilePath, targetFilePath, false, $"klasik işlem başarısız. Hata : {ex.Message}");
+                }
+            }
+        }
+
+        private void SaveSettings(string sessionanadizin, string sessiontargetpath, bool isYenidenAdlandir, bool isAltDizin)
+        {
+            // app.confige yazan fonksiyon eğer kabul edilirse işlem gerçekleşir ve yeniden başlatıldığında en son yüklenen değerleri alır.
+            //If the function that writes app.confige is accepted, the process is executed and when it is restarted, it gets the last loaded values.
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["SourceFile"].Value = sessionanadizin;
+            config.AppSettings.Settings["TargetFile"].Value = sessiontargetpath;
+            config.AppSettings.Settings["IsYenidenAdlandir"].Value = isYenidenAdlandir.ToString();
+            config.AppSettings.Settings["IsAltDizin"].Value = isAltDizin.ToString();
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private void myloggertxt(string zipFilePath, string targetFilePath, bool status, string errorMessage)
+        {
+            // loglama fonksiyonu çağırdığımız yerde logs klasörü yoksa oluşturur varsa içindeki logs dosyasını günceller.
+            // If there is no logs folder where we call the logging function, it creates it and updates the logs file if it exists.
+
+            string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + @"\logs\");
+
+            string logFilePath = Path.Combine(folderPath + "logs.txt");
+            if (!File.Exists(logFilePath))
+            {
+                if (!File.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                    File.Create(logFilePath).Dispose();
+                }
+                else
+                {
+                    File.Create(logFilePath).Dispose();
+                }
+            }
+
+            // .txt dosyasına yazan kısım
+            //The part that writes to the .txt file
+
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                string logMessage = $"{DateTime.Now}: SourceFile={zipFilePath}, TargetFile={targetFilePath}, Status={status}";
+                if (status)
+                {
+                    logMessage += $", Mesaj = {errorMessage}";
+                }
+                else
+                {
+                    logMessage += $", Hata Mesajı = {errorMessage}";
+                }
+                writer.WriteLine(logMessage);
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // burası form yüklenirken önceden kayıt edilen dosya var mı diye app.config dosyasını okuyor
+            //This is where it reads the app.config file to see if there is a pre-registered file while the form is being loaded.
+
+            zipLevel.SelectedIndex = 1;
+            sourceText.Text = ConfigurationManager.AppSettings["SourceFile"];
+            targetText.Text = ConfigurationManager.AppSettings["TargetFile"];
+
+            bool isYenidenAdlandir = Convert.ToBoolean(ConfigurationManager.AppSettings["IsYenidenAdlandir"]);
+            bool isAltDizin = Convert.ToBoolean(ConfigurationManager.AppSettings["IsAltDizin"]);
+
+            rename.Checked = isYenidenAdlandir;
+            subdirectorieszipped.Checked = isAltDizin;
+        }
+    }
+}
